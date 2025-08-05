92f2838a71efe94b38fc2089d03a6d6b9ad763a7
Yashveer singh <yashveersingh1921@gmail.com>
2025-08-05 14:50:33 +0530

    frontend updated

---
+++ FRONTEND/src/components/CreateTodo.jsx
@@ -0,0 +1,7 @@
+export function CreateTodo(){
+    return <div>
+        <input type="text" placeholder="Title" /> <br />
+        <input type="text" placeholder="Description" /> <br />
+        <button>Add a todo</button>
+    </div>
+}
